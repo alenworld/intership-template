d6b173277db7643b54c12f2b8996cfaf8dc9163f
alenworld <inalipa.alen@gmail.com>
2022-01-11 19:13:38 +0200

    init

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -36,55 +36,16 @@ const chatRoom = async (req, res, next) => {
 };
 
 const socketEvents = (io) => {
-  const users = [];
-  let fullNames;
-  let clients = 0;
-
-  const getConnectedPeople = () => {
-    fullNames = Object.values(users);
-    console.log(fullNames);
-    return fullNames;
-  };
-
   io.on('connection', (socket) => {
-    clients++;
-    socket.emit('newclientconnect', { description: 'Hey, welcome!' });
-    socket.broadcast.emit('newclientconnect', { description: `${clients} clients connected!` });
+    // Welcome current user
+    socket.emit('message', 'Welcome to ChatCord!');
 
-    console.info(`New socket connection: ${socket.id}`);
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat!');
 
-    socket.on('new-user', (fullName) => {
-      users[socket.id] = fullName;
-      socket.broadcast.emit('new-user-message', fullName);
-      socket.broadcast.emit('users-list', getConnectedPeople());
-      socket.emit('users-list', fullNames);
-      console.info(`${fullName} joined the chat!`);
-    });
-
-    socket.on('send-chat-message', (message) => {
-      if (users[socket.id] == null) {
-        socket.emit('reload');
-        return;
-      }
-      socket.broadcast.emit('chat-message', {
-        message,
-        firstName: users[socket.id].firstName,
-      });
-    });
-
-    socket.on('user-typing', (fullName) => {
-      socket.broadcast.emit('user-typing', fullName);
-    });
-
+    // Runs when client disconnects
     socket.on('disconnect', () => {
-      clients--;
-      if (users[socket.id] !== undefined && users[socket.id] !== null) {
-        socket.broadcast.emit('disconnect-message', users[socket.id]);
-      }
-      socket.broadcast.emit('broadcast', { description: `${clients} clients connected` });
-      console.log(`${users[socket.id]} left the chat!`);
-      delete users[socket.id];
-      socket.broadcast.emit('users-list', getConnectedPeople());
+      io.emit('message', 'A user has left the chat!');
     });
   });
 };
